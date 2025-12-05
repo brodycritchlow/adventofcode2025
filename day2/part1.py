fb0d05ba0edb9965c368fb329312ffe9e3b8c1b7
Brody Critchlow <brodycritchlow@gmail.com>
2025-12-05 04:47:30 -0700

    make part1 of day2

--- day2/part1.py
+++ day2/part1.py
@@ -4,8 +4,26 @@ grab_input("https://adventofcode.com/2025/day/2/input")
 
 
 def main():
+    invalid_ids = 0
     with open("input.txt", "r") as f:
-        pass
+        for range_ in f.readline().split(","):
+            firstNum, secondNum = range_.split("-")
+            fint, sint = int(firstNum), int(secondNum)
+
+            for i in range(fint, sint + 1):
+                si = str(i)
+
+                if len(si) % 2 != 0:
+                    # we know its valid since we cant split into two even pieces
+                    continue
+
+                first_half = si[0 : len(si) // 2]
+                second_half = si[len(si) // 2 : len(si)]
+
+                if first_half == second_half:
+                    invalid_ids += int(i)
+
+    return invalid_ids
 
 
 if __name__ == "__main__":
