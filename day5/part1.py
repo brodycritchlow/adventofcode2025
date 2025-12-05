5913bc4a43e8ca52ea8101286e7aa5c8d7268bd7
Brody Critchlow <brodycritchlow@gmail.com>
2025-12-05 04:11:21 -0700

    Setup input grabbing + main function in all

---
+++ day5/part1.py
@@ -0,0 +1,12 @@
+from grab_input import grab_input
+
+grab_input("https://adventofcode.com/2025/day/5/input")
+
+
+def main():
+    with open("input.txt", "r") as f:
+        pass
+
+
+if __name__ == "__main__":
+    print(main())
