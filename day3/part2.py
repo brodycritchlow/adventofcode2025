1c86cd314a0aa3795b8c7c9f379a0a671ae406b6
Brody Critchlow <brodycritchlow@gmail.com>
2025-12-10 02:14:26 -0700

    day3 part2

--- day3/part2.py
+++ day3/part2.py
@@ -1,11 +1,28 @@
 from grab_input import grab_input
+from itertools import combinations
 
 grab_input("https://adventofcode.com/2025/day/3/input")
 
+# This runs extremely slow, but I cant be asked to optimize it
+
 
 def main():
+    max_num = 0
+    total = 0
+
     with open("input.txt", "r") as f:
-        pass
+        for line in f.readlines():
+            line = line.strip()
+            max_num = 0
+
+            for i in combinations(line, r=12):
+                num = int("".join(i))
+                if num > max_num:
+                    max_num = num
+
+            total += max_num
+
+    return total
 
 
 if __name__ == "__main__":
