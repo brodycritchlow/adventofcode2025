3289ab7dddd789ea90bc5f7147fc88607c6f37b6
Brody Critchlow <brodycritchlow@gmail.com>
2025-12-10 01:47:34 -0700

    day3 part 1

--- day3/part1.py
+++ day3/part1.py
@@ -4,8 +4,24 @@ grab_input("https://adventofcode.com/2025/day/3/input")
 
 
 def main():
+    max_num = 0
+    total = 0
+
     with open("input.txt", "r") as f:
-        pass
+        for line in f.readlines():
+            line = line.strip()
+            max_num = 0
+
+            for idx, i in enumerate(line):
+                for j in line[idx + 1 :]:
+                    num = int(i + j)
+
+                    if num > max_num:
+                        max_num = num
+
+            total += max_num
+
+    return total
 
 
 if __name__ == "__main__":
