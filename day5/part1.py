acd7eb4e26a07b334a847cb50b84654e6227276a
Brody Critchlow <brodycritchlow@gmail.com>
2025-12-11 04:05:33 -0700

    day5 part1

--- day5/part1.py
+++ day5/part1.py
@@ -4,8 +4,28 @@ grab_input("https://adventofcode.com/2025/day/5/input")
 
 
 def main():
+    fresh = 0
+    seen = set()
+
     with open("input.txt", "r") as f:
-        pass
+        ranges, available = f.read().split("\n\n")
+
+        for number in available.splitlines():
+            n = int(number)
+
+            if number in seen:
+                continue
+
+            for range in ranges.splitlines():
+                a, b = range.split("-")
+                a, b = int(a), int(b)
+
+                if a <= n <= b:
+                    fresh += 1
+                    seen.add(number)
+                    break
+
+    return fresh
 
 
 if __name__ == "__main__":
