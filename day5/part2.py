16ca8515b54303b29eb2b821fa5076a9f92f1b54
Brody Critchlow <brodycritchlow@gmail.com>
2025-12-11 04:15:02 -0700

    day5 part2

--- day5/part2.py
+++ day5/part2.py
@@ -5,7 +5,28 @@ grab_input("https://adventofcode.com/2025/day/5/input")
 
 def main():
     with open("input.txt", "r") as f:
-        pass
+        ranges, available = f.read().split("\n\n")
+
+        range_list = []
+        for range_ in ranges.splitlines():
+            a, b = range_.split("-")
+            a, b = int(a), int(b)
+            range_list.append((a, b))
+
+        range_list.sort()
+
+        merged = []
+        for start, end in range_list:
+            if merged and start <= merged[-1][1] + 1:
+                merged[-1] = (merged[-1][0], max(merged[-1][1], end))
+            else:
+                merged.append((start, end))
+
+        count = 0
+        for start, end in merged:
+            count += end - start + 1
+
+    return count
 
 
 if __name__ == "__main__":
